9a685f70d4e4cb2110890d1e910a2ffa546ce2ee
寒露 <hanlu@example.com>
2026-03-12 20:23:41 +0800

    修复模块导入问题：移除通配符导入，改为显式导入

--- ssh_executor/__init__.py
+++ ssh_executor/__init__.py
@@ -2,8 +2,64 @@
 SSH 执行器包
 """
 
-from .core import *
-from .models import *
-from .exceptions import *
+# 不进行通配符导入，避免导入问题
+# from .core import *
+# from .models import *
+# from .exceptions import *
+
+# 显式导入核心类
+from ssh_executor.core.connection import SSHConnection, SSHConnectionManager, get_global_manager, shutdown_global_manager
+from ssh_executor.models.config import (
+    SSHConnectionConfig, 
+    SSHAuthConfig, 
+    AuthType, 
+    ConnectionStatus, 
+    CommandResult
+)
+from ssh_executor.exceptions import (
+    SSHExecutorError,
+    SSHConnectionError,
+    SSHAuthenticationError,
+    SSHTimeoutError,
+    SSHCommandError,
+    SSHConfigError,
+    SSHConnectionPoolError,
+    SSHKeyError,
+    SSHPermissionError,
+    SSHNetworkError,
+    SSHRetryExhaustedError,
+    SSHHostKeyError,
+    SSHCommandTimeoutError,
+)
 
 __version__ = "0.1.0"
+
+__all__ = [
+    # Core
+    "SSHConnection",
+    "SSHConnectionManager",
+    "get_global_manager",
+    "shutdown_global_manager",
+    
+    # Models
+    "SSHConnectionConfig",
+    "SSHAuthConfig",
+    "AuthType",
+    "ConnectionStatus",
+    "CommandResult",
+    
+    # Exceptions
+    "SSHExecutorError",
+    "SSHConnectionError",
+    "SSHAuthenticationError",
+    "SSHTimeoutError",
+    "SSHCommandError",
+    "SSHConfigError",
+    "SSHConnectionPoolError",
+    "SSHKeyError",
+    "SSHPermissionError",
+    "SSHNetworkError",
+    "SSHRetryExhaustedError",
+    "SSHHostKeyError",
+    "SSHCommandTimeoutError",
+]
